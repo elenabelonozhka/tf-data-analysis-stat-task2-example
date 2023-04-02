6ac84fa3e0d79e7d0a7669bb0c25f555ab4b06f5
elenabelonozhka <64587763+elenabelonozhka@users.noreply.github.com>
2023-04-02 13:55:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,12 +3,12 @@ import numpy as np
 
 from scipy.stats import norm
 from scipy.stats import chi2
-from math import sqrt
+
 
 
 chat_id = 728846853 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
   n = len(x)
-  return sqrt(sum(x**2)/ (6*chi2.ppf((1+p)/2, df = n))), \
-  sqrt(sum(x**2)/ (6*chi2.ppf((1 - p) / 2, df = n ))) 
+  return np.sqrt(sum(x**2)/ (6*chi2.ppf((1+p)/2, df = n))), \
+  np.sqrt(sum(x**2)/ (6*chi2.ppf((1 - p) / 2, df = n ))) 
